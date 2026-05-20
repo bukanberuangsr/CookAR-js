dbaf22ec350553bcf60701d091fcf06e4a86fcd0
bukanberuangsr <akhfizpuli@gmail.com>
2026-05-20 16:17:25 +0700

    add nasi lemak, update mi ayam model, and update model placement

--- scripts/foods.js
+++ scripts/foods.js
@@ -67,5 +67,34 @@ const foods = {
         ],
         cookingProcess: "Direbus dan ditumis",
         model: "./assets/models/mangkuk_bakso_mie_ayam.glb"
+    },
+    nasiLemak: {
+        name: "Nasi Lemak",
+        ingredients: [
+            "Nasi",
+            "Daun salam",
+            "Sereh",
+            "Lengkuas",
+            "Garam",
+            "Jahe",
+            "Santan",
+            "Ayam berbumbu",
+            "Telur",
+            "Ikan Asin",
+            "Kacang"
+        ],
+        nutrition: [
+            "Kalori: 800 kkal",
+            "Karbohidrat: 55 g",
+            "Lemak: 14 g",
+            "Protein: 20 g"
+        ],
+        allergens: [
+            "Kacang",
+            "Telur",
+            "Kedelai"
+        ],
+        cookingProcess: "Direbus",
+        model: "./assets/models/nasi_lemak.glb"
     }
 };
